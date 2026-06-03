cd4aa01ea07a8e9b0eb385d0abd9d689039eff78
KMRa7 <y.kimura11223@gmail.com>
2026-06-03 16:47:15 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-
 const app = process.env.VITE_APP || 'clockin'
-
 export default defineConfig({
   plugins: [react()],
-  base: `/${app === 'clockin' ? 'clockin' : 'derby-clockin'}/`,
   resolve: {
     alias: {
       '@config': path.resolve(__dirname, `apps/${app}/config.js`),
